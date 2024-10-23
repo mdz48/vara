b177ba621860899c8a82391dbcc4ce341fee970d
mdz48 <maximilianodiazclemen.cetis138@gmail.com>
2024-10-23 16:01:31 -0600

    INITIAL

--- frontend/src/app/consts.ts
+++ frontend/src/app/consts.ts
@@ -18,66 +18,102 @@ export const ROUTES = {
 };
 
 // To use the example code, enter the details of the account that will pay the vouchers, etc. (name and mnemonic)
-export const sponsorName = "";
-export const sponsorMnemonic = "";
+export const sponsorName = "Alice";
+export const sponsorMnemonic = "bottom drive obey lake curtain smoke basket hold race lonely fit walk";
 
 export const CONTRACT_DATA: ContractSails = {
-  programId: '0x285b178b5ec7720e8ae614e2dc032b86c0135e8a8579fe80c07ab655062e791b',
+  programId: '0x484313fec847994255f3ab1bdd9d243e80732d3e12b63555c7539a313594fbcb',
   idl: `
     type KeyringData = struct {
-      address: str,
-      encoded: str,
-    };
+  address: str,
+  encoded: str,
+};
 
-    type KeyringEvent = enum {
-      KeyringAccountSet,
-      Error: KeyringError,
-    };
+type KeyringEvent = enum {
+  KeyringAccountSet,
+  Error: KeyringError,
+};
 
-    type KeyringError = enum {
-      KeyringAddressAlreadyEsists,
-      UserAddressAlreadyExists,
-      UserCodedNameAlreadyExists,
-      UserDoesNotHasKeyringAccount,
-      KeyringAccountAlreadyExists,
-      SessionHasInvalidCredentials,
-      UserAndKeyringAddressAreTheSame,
-    };
+type KeyringError = enum {
+  KeyringAddressAlreadyEsists,
+  UserAddressAlreadyExists,
+  UserCodedNameAlreadyExists,
+  UserDoesNotHasKeyringAccount,
+  KeyringAccountAlreadyExists,
+  SessionHasInvalidCredentials,
+  UserAndKeyringAddressAreTheSame,
+};
 
-    type KeyringQueryEvent = enum {
-      LastWhoCall: actor_id,
-      SignlessAccountAddress: opt actor_id,
-      SignlessAccountData: opt KeyringData,
-    };
+type KeyringQueryEvent = enum {
+  LastWhoCall: actor_id,
+  SignlessAccountAddress: opt actor_id,
+  SignlessAccountData: opt KeyringData,
+};
 
-    type TrafficLightEvent = enum {
-      Green,
-      Yellow,
-      Red,
-    };
+type PersonReport = struct {
+  firstname_denuncied: str,
+  lastname_denuncied: str,
+  img_denuncied: str,
+  description_denuncied: str,
+  age_denuncied: u128,
+};
 
-    type IoTrafficLightState = struct {
-      current_light: str,
-      all_users: vec struct { actor_id, str },
-    };
+type Category = enum {
+  FaltasALaProbidadAdministrativa,
+  MalUsoDeRecursos,
+  Robo,
+  TraficoDeInfluencias,
+  Maltrato,
+  AcosoLaboral,
+  AcosoSexual,
+  DiscriminacionArbitraria,
+  ConflictoDeInteres,
+  UsoDeInformacionReservada,
+  Soborno,
+  IntentoDeAsesinato,
+  AgresionFisica,
+  AgresionVerbal,
+  Violacion,
+  OperacionSospechosa,
+  Estafa,
+  Amenaza,
+  Otros,
+};
 
-    constructor {
-      New : ();
-    };
+type ReportDataMent = struct {
+  person_report: PersonReport,
+  report_id: u128,
+  details: str,
+  is_public: bool,
+  description: str,
+  category: Category,
+  otro: opt str,
+};
 
-    service Keyring {
-      BindKeyringDataToUserAddress : (user_address: actor_id, keyring_data: KeyringData) -> KeyringEvent;
-      BindKeyringDataToUserCodedName : (user_coded_name: str, keyring_data: KeyringData) -> KeyringEvent;
-      query KeyringAccountData : (keyring_address: actor_id) -> KeyringQueryEvent;
-      query KeyringAddressFromUserAddress : (user_address: actor_id) -> KeyringQueryEvent;
-      query KeyringAddressFromUserCodedName : (user_coded_name: str) -> KeyringQueryEvent;
-    };
+type Enterprise = struct {
+  name: str,
+  code_post: str,
+  location: str,
+  hour: str,
+  name_owner: str,
+};
 
-    service TrafficLight {
-      Green : () -> TrafficLightEvent;
-      Red : () -> TrafficLightEvent;
-      Yellow : () -> TrafficLightEvent;
-      query TrafficLight : () -> IoTrafficLightState;
-    };
+constructor {
+  New : ();
+};
+
+service Keyring {
+  BindKeyringDataToUserAddress : (user_address: actor_id, keyring_data: KeyringData) -> KeyringEvent;
+  BindKeyringDataToUserCodedName : (user_coded_name: str, keyring_data: KeyringData) -> KeyringEvent;
+  query KeyringAccountData : (keyring_address: actor_id) -> KeyringQueryEvent;
+  query KeyringAddressFromUserAddress : (user_address: actor_id) -> KeyringQueryEvent;
+  query KeyringAddressFromUserCodedName : (user_coded_name: str) -> KeyringQueryEvent;
+};
+
+service ReportService {
+  CreatedReport : (person_report: PersonReport, report_id: u128, details: str, is_public: bool, description: str, category: Category, otro: opt str) -> ReportDataMent;
+  query GetAllEnterprises : () -> vec Enterprise;
+  query GetAllReports : () -> vec ReportDataMent;
+};
   `
 };
